ee11ad14a26f10843545b40f196fcc1701ae76eb
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-02-22 21:55:58 +0700

    Add Book, User struct and response

---
+++ entity/book.go
@@ -0,0 +1,10 @@
+package entity
+
+type Book struct {
+	ID          uint64 `gorm:"primary_key:auto_increment" json:"id"`
+	Title       string `gorm:"type:varchar(255)" json:"title"`
+	Author      string `gorm:"type:varchar(255)" json:"author"`
+	Description string `gorm:"type:text" json:"description"`
+	UserID      uint64 `gorm:"not null" json:"-"`
+	User        User   `gorm:"foreignKey:UserID;constraint:onUpdate:CASCADE,onDelete:CASCADE" json:"user"`
+}
---
+++ helper/response.go
@@ -0,0 +1,35 @@
+package helper
+
+import "strings"
+
+type Response struct {
+	Success bool   `json:"success"`
+	Message string `json:"message"`
+	// To return dynamic data
+	Error interface{} `json:"error"`
+	Data  interface{} `json:"data"`
+}
+
+type EmptyObject struct {
+}
+
+func SendSuccessResponse(success bool, message string, data interface{}) Response {
+	res := Response{
+		Success: true,
+		Message: message,
+		Error:   nil,
+		Data:    data,
+	}
+	return res
+}
+
+func SendErrorResponse(success bool, message string, err string, data interface{}) Response {
+	splitedError := strings.Split(err, "\n")
+	res := Response{
+		Success: false,
+		Message: message,
+		Error:   splitedError,
+		Data:    data,
+	}
+	return res
+}
