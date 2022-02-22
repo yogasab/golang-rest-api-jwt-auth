c48111b137b6010d2ba1446921210a649be44f22
Yoga Baskoro <baskoroyoga40@gmail.com>
2022-02-22 20:39:29 +0700

    Setting up database with gorm and mysql

---
+++ config/database.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func SetupDatabaseConnection() *gorm.DB {
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error while loading env files")
+	}
+	dbName := os.Getenv("DB_NAME")
+	dbUser := os.Getenv("DB_USER")
+	dbPort := os.Getenv("DB_PORT")
+	dbHost := os.Getenv("DB_HOST")
+
+	dsn := fmt.Sprintf("%s:@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", dbUser, dbHost, dbPort, dbName)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	//db.AutoMigrate()
+	return db
+}
+
+func CloseDatabaseConnection(db *gorm.DB) {
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Println("Error while close database connection")
+	}
+	sqlDB.Close()
+}
